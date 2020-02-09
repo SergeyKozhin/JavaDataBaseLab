fceb985db1e13c1a36247dd4f5fa563b02c911c1
Sergey Kozhin <kozhin.sv@edu.spbstu.ru>
2020-02-09 20:02:32 +0300

    Added Product class

---
+++ src/main/java/db/Product.java
@@ -0,0 +1,57 @@
+package db;
+
+import java.util.UUID;
+
+public class Product {
+    private int id;
+    private String prodId;
+    private String title;
+    private int cost;
+
+    public Product() {
+
+    }
+
+    public Product(int id, String prodId, String title, int cost) {
+        this.id = id;
+        this.prodId = prodId;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Product(String prodId, String title, int cost) {
+        this(0, prodId, title, cost);
+    }
+
+    public Product(String title, int cost) {
+        this(UUID.randomUUID().toString(), title, cost);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProdId() {
+        return prodId;
+    }
+
+    public void setProdId(String prodId) {
+        this.prodId = prodId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+}
